ee46b148bb43cc553ecfd3cdc3f695b41294491f
Tuan Vu <hoangtuanbs@yahoo.com>
2018-01-18 00:47:55 +0200

    Install edge script

---
+++ edge-install.sh
@@ -0,0 +1,9 @@
+sudo apt-get install python-pip -y
+
+sudo pip install -U azure-iot-edge-runtime-ctl
+
+sudo iotedgectl setup --connection-string "{$1}" --auto-cert-gen-force-no-passwords
+
+sudo iotedgectl start
+
+sudo docker ps
