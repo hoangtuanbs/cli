526394511f9494459cb589c09904996884a1e4e3
Tuan Vu <hoangtuanbs@yahoo.com>
2018-01-18 01:02:34 +0200

    Change parameters

--- edge-install.sh
+++ edge-install.sh
@@ -2,7 +2,7 @@ sudo apt-get install python-pip -y
 
 sudo pip install -U azure-iot-edge-runtime-ctl
 
-sudo iotedgectl setup --connection-string "{$1}" --auto-cert-gen-force-no-passwords
+sudo iotedgectl setup --connection-string $1 --auto-cert-gen-force-no-passwords
 
 sudo iotedgectl start
 
